f1d12004c8b780f252178a45f36dafaf7bb15cd8
sebastian.larrieu <sebastian.larrieu>
2022-11-09 12:35:40 -0300

    based on url

--- dashboard/src/dashboard.js
+++ dashboard/src/dashboard.js
@@ -1,11 +1,16 @@
-import('HelloWorldApp/HelloWorldPage').then((HelloWorldPageModule) => {
-  const HelloWorldPage = HelloWorldPageModule.default;
-  const helloWorldPage = new HelloWorldPage();
-  helloWorldPage.render();
-});
+const url = window.location.pathname;
 
-import('RomanApp/RomanPage').then((RomanPageModule) => {
-  const RomanPage = RomanPageModule.default;
-  const romanPage = new RomanPage();
-  romanPage.render();
-});
+console.log(url, url === '/hello');
+if (url === '/hello') {
+  import('HelloWorldApp/HelloWorldPage').then((HelloWorldPageModule) => {
+    const HelloWorldPage = HelloWorldPageModule.default;
+    const helloWorldPage = new HelloWorldPage();
+    helloWorldPage.render();
+  });
+} else if (url === '/roman') {
+  import('RomanApp/RomanPage').then((RomanPageModule) => {
+    const RomanPage = RomanPageModule.default;
+    const romanPage = new RomanPage();
+    romanPage.render();
+  });
+}
